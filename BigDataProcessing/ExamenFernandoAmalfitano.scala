930c4d8e86df56901ad5bee0f8ed974bff507b9d
Fernando <fernandoamalfitano@gmail.com>
2026-04-27 20:24:45 +0200

    Actualizo repo y añado practica de BigDataProcessing

---
+++ BigDataProcessing/ExamenFernandoAmalfitano.scala
@@ -0,0 +1,86 @@
+package examen_estructura
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.catalyst.dsl.expressions.{DslAttr, StringToAttributeConversionHelper}
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object Examen {
+
+  /** Ejercicio 1: Crear un DataFrame y realizar operaciones básicas
+   * Pregunta: Crea un DataFrame a partir de una secuencia de tuplas que contenga información sobre
+   * estudiantes (nombre, edad, calificación).
+   * Realiza las siguientes operaciones:
+   *
+   * Muestra el esquema del DataFrame.
+   * Filtra los estudiantes con una calificación mayor a 8.
+   * Selecciona los nombres de los estudiantes y ordénalos por calificación de forma descendente.
+   */
+
+  import org.apache.spark.sql.functions._
+  def ejercicio1(estudiantes: DataFrame)(implicit spark: SparkSession): DataFrame = {
+    val estudiantesDF: DataFrame = estudiantes.toDF("nombre", "edad", "nota")
+    val Schema = estudiantes.printSchema()
+    val AlumnosNotaMayorOcho = estudiantesDF.filter("nota > 8").orderBy(desc("nota"))
+    val NombreAlumnos = AlumnosNotaMayorOcho.select("nombre")
+    NombreAlumnos
+  }
+
+  /** Ejercicio 2: UDF (User Defined Function)
+   * Pregunta: Define una función que determine si un número es par o impar.
+   * Aplica esta función a una columna de un DataFrame que contenga una lista de números.
+   */
+
+  def ejercicio2(numeros: DataFrame)(implicit spark: SparkSession): DataFrame = {
+    val ParOimparDF = udf((numero: Int) =>
+      if (numero % 2 == 0) "Par" else "Impar")
+    val NewColum = numeros.withColumn("ParOimpar", ParOimparDF(col("numero")))
+    NewColum.select("ParOimpar")
+  }
+
+  /** Ejercicio 3: Joins y agregaciones
+   * Pregunta: Dado dos DataFrames,
+   * uno con información de estudiantes (id, nombre)
+   * y otro con calificaciones (id_estudiante, asignatura, calificacion),
+   * realiza un join entre ellos y calcula el promedio de calificaciones por estudiante.
+   */
+
+  def ejercicio3(estudiantes: DataFrame, calificaciones: DataFrame): DataFrame = {
+    val ResultadoJoin = estudiantes.join(calificaciones,
+      estudiantes("id") === calificaciones("id_estudiante"))
+      .groupBy(estudiantes("id"), estudiantes("nombre"))
+      .agg {
+        sum(calificaciones("calificacion")) / count(calificaciones("calificacion"))
+      }
+    ResultadoJoin
+  }
+
+  /** Ejercicio 4: Uso de RDDs
+   * Pregunta: Crea un RDD a partir de una lista de palabras y cuenta la cantidad de ocurrencias de cada palabra.
+   */
+
+  def ejercicio4(palabras: List[String])(implicit spark: SparkSession): RDD[(String, Int)] = {
+    val PalabrasRDD = spark.sparkContext.parallelize(palabras)
+    val OcurrenciasRDD = PalabrasRDD.map(palabra => (palabra, 1))
+      .reduceByKey(_ + _)
+    OcurrenciasRDD
+  }
+
+  /**
+   * Ejercicio 5: Procesamiento de archivos
+   * Pregunta: Carga un archivo CSV que contenga información sobre
+   * ventas (id_venta, id_producto, cantidad, precio_unitario)
+   * y calcula el ingreso total (cantidad * precio_unitario) por producto.
+   */
+  def ejercicio5(ventas: DataFrame)(implicit spark: SparkSession): DataFrame = {
+    ventas.createOrReplaceTempView("ventas")
+    val resultado = spark.sql("""
+      select id_producto,
+             SUM(precio_unitario * cantidad) as total_ventas
+      from ventas
+      group by id_producto
+    """)
+
+    resultado
+  }
+}
